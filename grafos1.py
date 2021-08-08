10449f2440af989901c2665a6776e3fe48170d0f
giovanadionisio <giovanadionisio.gd@gmail.com>
2021-08-07 23:56:17 -0300

    Calcula ciclos

--- grafos1.py
+++ grafos1.py
@@ -7,26 +7,32 @@ class Grafo(object):
 
     def __init__(self):
         self.adj = defaultdict(set)
+        self.n_nos = 0
+        self.ciclos = 0
+        self.n_contaminados = 0
+        self.contaminados = {}
         # self.adj = [[] for _ in range(10)]
 
+    def set_nnos(self, n):
+        self.n_nos = n
+
     def gerar_arestas(self):
-        nos = list(string.ascii_uppercase)
-        qtd_nos = random.randint(2, 26)
+        qtd_nos = 200
+        self.set_nnos(qtd_nos)
         print(qtd_nos)
         max_arestas = random.randint(qtd_nos-1, (qtd_nos*(qtd_nos-1))//2)
         print(max_arestas)
         arestas = []
-        print(nos)
+        # print(nos)
         for n in range(max_arestas):
             while True:
-                v = nos[random.randint(0, qtd_nos-1)]
-                u = nos[random.randint(0, qtd_nos-1)]
+                v = random.randint(0, qtd_nos-1)
+                u = random.randint(0, qtd_nos-1)
                 if v != u and tuple((v, u)) not in arestas:
                     arestas.append(tuple((v,u)))
                     break
-        print(arestas)
+        # print(arestas)
         return arestas
-
 
     def get_vertices(self):
         return list(self.adj.keys())
@@ -40,9 +46,39 @@ class Grafo(object):
 
     def adiciona_aresta_dupla(self, u, v):
         self.adj[u].add(v)
+        self.adj[v].add(u)
 
     def existe_aresta(self, u, v):
         return u in self.adj and v in self.adj[u]
+
+    def adiciona_contagio(self, u):
+        self.contaminados[u] = 'Contaminado'
+        self.n_contaminados += 1
+
+    def contamina_2(self):        
+        for n in self.contaminados.copy():
+            contagio = 0
+            for m in self.adj[n]:
+                if m not in self.contaminados:
+                    self.adiciona_contagio(m)
+                    contagio += 1
+                if contagio == 2:
+                    break
+        self.ciclos += 1
+
+    def contamina_1(self):        
+        for n in self.contaminados.copy():
+            for m in self.adj[n]:
+                if m not in self.contaminados:
+                    self.adiciona_contagio(m)
+                    break;
+        self.ciclos += 1
+    
+    def zerar_dados(self):
+        self.ciclos = 0
+        self.contaminados = {}
+        self.n_contaminados = 0
+        self.adiciona_contagio(1)
 
     def __len__(self):
         return len(self.adj)
@@ -56,8 +92,22 @@ class Grafo(object):
 
 if __name__ in "__main__":
     grafo = Grafo()
+    
     grafo.adiciona_arestas(grafo.gerar_arestas())
-    print(grafo.adj)
+    grafo.adiciona_contagio(1)
+    print()
+    
+    #print(grafo.adj)
+    while (grafo.n_contaminados != grafo.n_nos):
+        grafo.contamina_2()
+    print("Sem vacina: ","Número contaminados:", grafo.n_contaminados, "Ciclos:", grafo.ciclos)
+    #
+    print()
+    grafo.zerar_dados()
+    while (grafo.n_contaminados != grafo.n_nos):
+        grafo.contamina_1()
+    print("Com vacina: ","Número contaminados:", grafo.n_contaminados, "Ciclos:", grafo.ciclos)
+    
     #print(grafo.get_vertices())
     #print(grafo.get_arestas())
     #print(grafo.existe_aresta('A', 'B'), grafo.existe_aresta('E', 'C'))
